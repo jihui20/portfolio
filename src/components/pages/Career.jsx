81af7684eac1fa802fe81c5e664432e75b51a410
이지희 <dlwlgml20@gmail.com>
2023-01-02 00:27:27 +0900

    프로필 내 직무 추가

--- src/components/pages/Career.jsx
+++ src/components/pages/Career.jsx
@@ -62,22 +62,22 @@ export default function Career() {
       <CommonStyle.H3 showType="block">work experience</CommonStyle.H3>
       <CareerBox>
         <dt>
-          위메프 <span>2022년 6월 &ndash; 현재</span>
+          위메프 <span>2022년 6월 &ndash; 현재 &middot; Frontend Web Developer</span>
         </dt>
         <dd>
           위메프 메인 사이트의 운영 및 유지 보수 개발 진행중입니다.<br />
-          또한 기존 레거시 시스템을 React.js, Next.js 기반으로 개편 작업중이며, 회원가입 페이지를 담당하고 있습니다.
+          또한 기존 레거시 시스템을 React, Next.js 기반으로 개편 작업중이며, 회원가입 페이지를 담당하고 있습니다.
         </dd>
         <dt>
-          야나두 <span>2019년 6월 &ndash; 2022년 6월 (3년)</span>
+          야나두 <span>2019년 6월 &ndash; 2022년 6월 (3년) &middot; Frontend Web Developer &amp; UI Developer</span>
         </dt>
         <dd>
           메인 상품 및 이벤트 페이지 등 사이트 내 여러 페이지를 운영 및 개발하였고,
           부트스트랩을 이용하여 백오피스 사이트를 리뉴얼하였습니다. <br />
-          그리고 프론트엔드로 직무를 전환하여 야나두 메인 사이트를 React.js로 리뉴얼 작업 후 성공적으로 오픈하여 현재 서비스 중입니다.
+          그리고 프론트엔드로 직무를 전환하여 야나두 메인 사이트를 React로 리뉴얼 작업 후 성공적으로 오픈하여 현재 서비스 중입니다.
         </dd>
         <dt>
-          순수교육 <span>2018년 1월 &ndash; 2019년 6월 (1년 6개월)</span>
+          순수교육 <span>2018년 1월 &ndash; 2019년 6월 (1년 6개월) &middot; UI Developer</span>
         </dt>
         <dd>
           메인 화면, 기프트샵, 상품 구매, 이벤트 및 공지사항 게시판 페이지 등
